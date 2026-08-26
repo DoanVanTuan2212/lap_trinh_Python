053280b15b41c12633c0503db7ab17ee5aaa4581
DoanVanTuan2212 <2411062479@hunre.edu.vn>
2026-08-26 13:20:08 +0700

    Hoan thanh bai 02

--- lap_trinh_Python/bai01/bien.py
+++ lap_trinh_Python/bai01/bien.py
@@ -6,3 +6,4 @@ bien = 3.14
 print(bien, type(bien))
 bien = True
 print(bien, type(bien))
+#Trả lời câu hỏi:Vì Python là ngôn ngữ kiểu động (dynamic typing) nên một biến không cần khai báo kiểu dữ liệu cố định. Kiểu của biến được xác định dựa vào giá trị mà nó đang tham chiếu.
---
+++ lap_trinh_Python/bai02/hoat_dong_1_2.py
@@ -0,0 +1,39 @@
+ho_ten = input("Nhap ho ten: ")
+nam_sinh = int(input("Nhap nam sinh: "))
+diem_tb = float(input("Nhap diem trung binh: "))
+#Yêu cầu: giải thích vì sao phải ép kiểu int()/float() cho nam_sinh và diem_tb, trong khi ho_ten thì không
+#ho_ten: là chuỗi ký tự (str) nên dữ liệu nhập vào từ input() đã đúng kiểu cần dùng → không cần ép kiểu.
+#nam_sinh: cần thực hiện các phép tính hoặc so sánh với số → phải chuyển từ chuỗi sang số nguyên bằng int().
+#diem_tb: có thể chứa số thập phân (ví dụ 8.5) → phải chuyển sang số thực bằng float().
+
+print("Python", "la", "ngon", "ngu", "lap trinh", sep="\n")
+print("Dong 1", end=" | ")
+print("Dong 2")
+#Yêu cầu: thử đổi sep thành nhiều ký tự khác nhau (", ", "\n") và quan sát kết quả rồi giải thích.
+#sau khi đổi các kí tự thì giữa các từ sẽ thay đổi theo như ví dụ \n thì giữa các từ sẽ xuống dòng
+
+# f-string
+print(f"Ho ten: {ho_ten} - Nam sinh: {nam_sinh} - DTB: {diem_tb:.2f}")
+# str.format()
+print("Ho ten: {} - Nam sinh: {} - DTB: {:.2f}".format(ho_ten, nam_sinh, diem_tb))
+# toán tử %
+
+print("Ho ten: %s - Nam sinh: %d - DTB: %.2f" % (ho_ten, nam_sinh, diem_tb))
+#Vì f-string ngắn gọn, dễ đọc và dễ viết hơn khi chèn biến vào chuỗi.
+
+#Hoạt động 2
+# Chu thich mot dong: khai bao thong tin sinh vien
+"""
+Chu thich/docstring nhieu dong:
+Chuong trinh quan ly diem sinh vien - Buoi 2
+"""
+ho_ten = "Tran Thi B" # bien luu ho ten
+s1 = 'Xin chao'
+s2 = "Ban co khoe khong?"
+s3 = '''Day la
+mot chuoi
+nhieu dong'''
+s4 = "Duong dan: C:\\Python\\data"
+s5 = r"Duong dan raw: C:\Python\data"
+s6 = "Toi ten la \"Nam\", con ban ten gi?"
+print(s1); print(s2); print(s3); print(s4); print(s5); print(s6)
